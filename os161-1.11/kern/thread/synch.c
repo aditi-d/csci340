7f8edda158a054dd85ae678515b7cf4b43018a7e
Aditi R. Deshpande <adeshpande@jaguar.ecst.csuchico.edu>
2013-03-30 17:39:56 -0700

    Removed test comments

--- os161-1.11/kern/thread/synch.c
+++ os161-1.11/kern/thread/synch.c
@@ -153,7 +153,7 @@ lock_acquire(struct lock *lock)
             thread_sleep(lock);
         }
     assert(lock->lockcurthread!=curthread);
-    kprintf("\nlock acquired:: %s",lock->name);
+    //kprintf("\nlock acquired:: %s",lock->name);
     lock->lockcurthread=curthread;
     splx(spl);
 	//(void)lock;  
@@ -172,7 +172,7 @@ lock_release(struct lock *lock)
     assert(lock!=NULL);
     spl=splhigh();
     assert(lock->lockcurthread==curthread);
-    kprintf("\nlock released:: %s",lock->name);
+    //kprintf("\nlock released:: %s",lock->name);
     lock->lockcurthread=NULL;
     thread_wakeup(lock);
     splx(spl);
